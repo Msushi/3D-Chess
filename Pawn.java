83f54b883ebc149d404ff374898a261315239db9
Msushi <39556839+Msushi@users.noreply.github.com>
2018-05-29 11:28:08 -0400

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -12,6 +12,7 @@ public class Pawn extends ChessPiece {
         if (this.teamColor == Color.WHITE) {
             if (isFirstMove) { //On the first move, the pawn can move 2 spaces forward
                 if (this.pos[1] == c.pos[1] && this.pos[2] == c.pos[2] && this.pos[0] - c.pos[0] == -1 || this.pos[0] - c.pos[0] == -2) {
+                    this.pos = c.pos;
                     isFirstMove = false;
                     return true;
                 }
@@ -24,6 +25,7 @@ public class Pawn extends ChessPiece {
         else {//if (this.teamColor == Color.BLACK) 
             if (isFirstMove) { //On the first move, the pawn can move 2 spaces forward
                 if (this.pos[1] == c.pos[1] && this.pos[2] == c.pos[2] && this.pos[0] - c.pos[0] == 1 || this.pos[0] - c.pos[0] == 2) {
+                    this.pos = c.pos;
                     isFirstMove = false;
                     return true;
                 }
@@ -34,8 +36,26 @@ public class Pawn extends ChessPiece {
              
         }
         //Checking for a valid capture move
-        if ()
-        
+        if (this.teamColor == Color.WHITE) {
+            //Checking for normal two dimensional capture
+            if (this.pos[2] == c.pos[2] && this.pos[0] == c.pos[0] - 1 && (this.pos[1] == c.pos[1] + 1 || this.pos[1] == c.pos[1] + 1))
+                //Checking that the square has a piece that belongs to the opponent
+                if (c != null && c.teamColor != this.teamColor) {
+                    this.pos = c.pos;
+                    c = null;
+                    return true;
+                }
+        }
+        else { //if  (this.teamColor == Color.BLACK); 
+            //Checking for normal two dimensional capture
+            if (this.pos[2] == c.pos[2] && this.pos[0] == c.pos[0] - 1 && (this.pos[1] == c.pos[1] - 1 || this.pos[1] == c.pos[1] + 1))
+                //Checking that the square has a piece that belongs to the opponent
+                if (c != null && c.teamColor != this.teamColor) {
+                    this.pos = c.pos;
+                    c = null;
+                    return true;
+                }
+        }
             
         
     }
