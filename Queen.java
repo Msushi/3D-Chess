18afabdb24cec7177cce5906f05dc1f7583aea27
spicyboi-felixeus52 <albert.y.guo@gmail.com>
2018-05-29 12:16:13 -0400

    queen praise the LAWD and all OMINSCIENCE ABOVE

---
+++ Queen.java
@@ -0,0 +1,136 @@
+public class Rook extends ChessPiece{
+
+    public Rook(Color color){
+        super(color, "Rook");
+        setIcon(//you know);
+        //BLAH BLAH bLAH
+    }
+
+    public boolean move(ChessPiece c){
+        //Checks if moving to original space
+        boolean rookBeValid = true;
+        boolean bishopBeValid = true;
+        
+        if(c.pos[0] == this.pos[0] && c.pos[1] == this.pos[1] && c.pos[2] == this.pos[2]){
+            rookBeValid = false;
+            break;
+        }
+        //Checks if position is in range; checks whether or not pieces are blocking the way
+        if(c.pos[0] == this.pos[0] && c.pos[1] == this.pos[1]){
+            if(c.pos[2] < this.pos[2]){
+                for(int i = this.pos[2]; i > c.pos[2]; i--){
+                    if(!board[c.pos[0]][c.pos[1]][i].equals(null)){
+                        rookBeValid = false;
+                        break;
+                    }
+                }
+            }
+            else{
+                for(int i = this.pos[2]; i < c.pos[2]; i++){
+                    if(!board[c.pos[0]][c.pos[1]][i].equals(null)){
+                        rookBeValid = false;
+                        break;
+                    }
+                }
+            }
+        }
+        else if(c.pos[1] == this.pos[1] && c.pos[2] == this.pos[2]){
+            if(c.pos[0] < this.pos[0]){
+                for(int i = this.pos[0]; i > c.pos[0]; i--){
+                    if(!board[i][c.pos[1]][c.pos[2]].equals(null)){
+                        rookBeValid = false;
+                        break;
+                    }
+                }
+            }
+            else{
+                for(int i = this.pos[0]; i < c.pos[0]; i++){
+                    if(!board[i][c.pos[1]][c.pos[2]].equals(null)){
+                        rookBeValid = false;
+                        break;
+                    }
+                }
+            }
+        }
+        else if(c.pos[0] == this.pos[0] && c.pos[2] == this.pos[2]){
+            if(c.pos[1] < this.pos[1]){
+                for(int i = this.pos[1]; i > c.pos[1]; i--){
+                    if(!board[c.pos[0]][i][c.pos[2]].equals(null)){
+                        rookBeValid = false;
+                        break;
+                    }
+                }
+            }
+            else{
+                for(int i = this.pos[1]; i < c.pos[1]; i++){
+                    if(!board[c.pos[0]][i][c.pos[2]].equals(null)){
+                        rookBeValid = false;
+                        break;
+                    }
+                }
+            }
+        }
+        else{
+            rookBeValid = false;
+        }
+         //Checks if position is in range; checks whether or not pieces are blocking the way
+        if(c.pos[0] - this.pos[0] == c.pos[1] - this.pos[1]){
+            if(c.pos[0] > this.pos[0]){
+                for(int i = 1; i < c.pos[0] - this.pos[0]; i++){
+                    if(!board[this.pos[0] + i][this.pos[1] + i][i].equals(null)){
+                        bishopBeValid = false;
+                        break;
+                    }
+                }
+            }
+            else{
+                for(int i = 1; i < this.pos[0] - c.pos[0]; i++){
+                    if(!board[this.pos[0] + i][this.pos[1] + i][i].equals(null)){
+                        bishopBeValid = false;
+                        break;
+                    }
+                }
+            }
+        }
+        else if(c.pos[0] - this.pos[0] == this.pos[1] - c.pos[1]){
+            if(c.pos[0] > this.pos[0]){
+                for(int i = 1; i < c.pos[0] - this.pos[0]; i++){
+                    if(!board[this.pos[0] + i][this.pos[1] - i][i].equals(null)){
+                        bishopBeValid = false;
+                        break;
+                    }
+                }
+            }
+            else{
+                for(int i = 1; i < this.pos[0] - c.pos[0]; i++){
+                    if(!board[this.pos[0] + i][this.pos[1] - i][i].equals(null)){
+                        bishopBeValid = false;
+                        break;
+                    }
+                }
+            }
+        }
+        else{
+            bishopBeValid = false;
+            break;
+        }
+        if(!bishopBeValid && !rookBeValid){
+            return false;
+        }
+        
+        //If existing piece
+        if((c != null) && (c.teamColor == this.teamColor)){
+            return false;
+        }
+        
+        //Removes chesspiece
+        if(c != null){
+            c = null;
+        }
+        //Updates position
+        this.pos[0] = c.pos[0];
+        this.pos[1] = c.pos[1];
+        this.pos[2] = c.pos[2];
+        return true;
+    }
+}
