ab23812fbb5c416eb94dc617fb3b5913227b5ca9
Msushi <39556839+Msushi@users.noreply.github.com>
2018-05-29 12:37:32 -0400

    Update King.java

--- King.java
+++ King.java
@@ -1,9 +1,24 @@
+import java.util.*;
 public class King extends ChessPiece {
   
-  public King(Color c) {
-    super (c, "King");
-    setIcon(king.jpg);
-  }
+    public King(Color c) {
+        super (c, "King");
+        setIcon(king.jpg);
+    }
   
-  public boolean move (ChessPiece c) {
-  
+    public boolean move (ChessPiece c) {
+        if (Arrays.equals(this.pos, c.pos)) //If inputted move is the current square
+            return false;
+        if (Math.abs(this.pos[0] - c.pos[0]) <= 1 && Math.abs(this.pos[1] - c.pos[1]) <= 1 && Math.abs(this.pos[2] - c.pos[2]) <= 1) {
+            if (c != null && c.teamColor == this.teamColor) //If a friednly piece occupies the square
+                return false;
+            else if (c != null && c.teamColor != this.teamColor) //If capture
+                c = null;
+            this.pos = c.pos;
+            return true;
+        }
+        return false;
+    }
+}
+            
+      
