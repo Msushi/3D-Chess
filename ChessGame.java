200b3a691359aaca99841f8418495389648e4310
spicyboi-felixeus52 <albert.y.guo@gmail.com>
2018-05-30 10:29:56 -0400

    Update ChessGame.java

--- ChessGame.java
+++ ChessGame.java
@@ -9,8 +9,10 @@ public class ChessGame {
     public ChessGame() {
         board = new ChessPiece[8][8][8];
         
-        //Initializes player 1's panws
-        for (int i = 0; i < 10;i++) {
+        //TODO: Initialise player objects to have colours
+        
+        //Initializes player 1's pawns
+        for (int i = 0; i < 8;i++) {
             board[1][i][0] = new Pawn(p1.getColor());
         }
         //Initializes player 1's special pieces
@@ -23,11 +25,11 @@ public class ChessGame {
         board[0][3][0] = new Queen(p1.getColor());
         board[0][4][0] = new King(p1.getColor());
         
-        //Initializes player 2's panws
-        for (int i = 0; i < 10;i++) {
+        //Initializes player 2's pawns
+        for (int i = 0; i < 8; i++) {
             board[6][i][0] = new Pawn(p2.getColor());
         }
-        //Initializes player 1's special pieces
+        //Initializes player 2's special pieces
         board[7][0][0] = new Rook(p2.getColor());
         board[7][7][0] = new Rook(p2.getColor());
         board[7][1][0] = new Knight(p2.getColor());
@@ -36,7 +38,16 @@ public class ChessGame {
         board[7][5][0] = new Bishop(p2.getColor());
         board[7][3][0] = new Queen(p2.getColor());
         board[7][4][0] = new King(p2.getColor());
+       
+        //Sets chesspieces to contain position info
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                for(int k = 0; k < 8; k++){
+                    board[i][j][k].setPosition(i, j, k);
+                }
+            }
+        }
+        
+        
     }
-    
-    
 }
