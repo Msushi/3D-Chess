ed6772ae19f2fc20eeb6dedfba6b1fb81790326d
Msushi <39556839+Msushi@users.noreply.github.com>
2018-06-05 08:47:21 -0400

    Update ChessGame.java

--- ChessGame.java
+++ ChessGame.java
@@ -11,33 +11,33 @@ public class ChessGame {
         
         //TODO: Initialise player objects to have colours
         
-        //Initializes player 1's pawns
+        //Initializes blacks' pawns
         for (int i = 0; i < 8;i++) {
-            board[1][i][0] = new Pawn(p1.getColor());
+            board[1][i][0] = new Pawn(false);
         }
-        //Initializes player 1's special pieces
-        board[0][0][0] = new Rook(p1.getColor());
-        board[0][7][0] = new Rook(p1.getColor());
-        board[0][1][0] = new Knight(p1.getColor());
-        board[0][6][0] = new Knight(p1.getColor());
-        board[0][2][0] = new Bishop(p1.getColor());
-        board[0][5][0] = new Bishop(p1.getColor());
-        board[0][3][0] = new Queen(p1.getColor());
-        board[0][4][0] = new King(p1.getColor());
+        //Initializes blacks's special pieces
+        board[0][0][0] = new Rook(false);
+        board[0][7][0] = new Rook(false);
+        board[0][1][0] = new Knight(false);
+        board[0][6][0] = new Knight(false);
+        board[0][2][0] = new Bishop(false);
+        board[0][5][0] = new Bishop(false);
+        board[0][3][0] = new Queen(false);
+        board[0][4][0] = new King(false);
         
-        //Initializes player 2's pawns
+        //Initializes whites's pawns
         for (int i = 0; i < 8; i++) {
-            board[6][i][0] = new Pawn(p2.getColor());
+            board[6][i][0] = new Pawn(true);
         }
-        //Initializes player 2's special pieces
-        board[7][0][0] = new Rook(p2.getColor());
-        board[7][7][0] = new Rook(p2.getColor());
-        board[7][1][0] = new Knight(p2.getColor());
-        board[7][6][0] = new Knight(p2.getColor());
-        board[7][2][0] = new Bishop(p2.getColor());
-        board[7][5][0] = new Bishop(p2.getColor());
-        board[7][3][0] = new Queen(p2.getColor());
-        board[7][4][0] = new King(p2.getColor());
+        //Initializes whites's special pieces
+        board[7][0][0] = new Rook(true);
+        board[7][7][0] = new Rook(true);
+        board[7][1][0] = new Knight(true);
+        board[7][6][0] = new Knight(true);
+        board[7][2][0] = new Bishop(true);
+        board[7][5][0] = new Bishop(true);
+        board[7][3][0] = new Queen(true);
+        board[7][4][0] = new King(true);
        
         //Sets chesspieces to contain position info
         for(int i = 0; i < 8; i++){
