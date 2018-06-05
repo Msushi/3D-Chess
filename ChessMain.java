c091ccd8a5a58f4ff6eed5c556d1debae3425a28
Msushi <39556839+Msushi@users.noreply.github.com>
2018-06-05 07:51:48 -0400

    Create ChessMain.java

---
+++ ChessMain.java
@@ -0,0 +1,8 @@
+public class ChessMain {
+    public static void main(String args[]) {
+        ChessFrame yeet = new ChessFrame();
+        yeet.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        yeet.setSize(500, 500);
+        yeet.setVisable();
+    }
+}
